aa223b3603d33864898237957944a1fc8d273d68
Aaron Krause <aaronjkrause@gmail.com>
2017-03-16 13:33:08 -0700

    starter code

---
+++ starter-code/public/test/test.js
@@ -0,0 +1,25 @@
+QUnit.module('Article Constructor', function() {
+  QUnit.test('Article should create a new Arcticle', function(assert) {
+    //TODO: write a test for the Article constructor
+  });
+
+  QUnit.test('Article instance should have a method toHtml', function(assert) {
+    //TODO: verify that an instance of article has a toHtml method
+  });
+});
+
+QUnit.module('Article Class', function() {
+  QUnit.test('Article.allArticles should be an array', function(assert) {
+    //TODO: write a test for the all articles property
+  });
+  QUnit.test('Article.loadAll should set an array of Article instances', function(assert) {
+    //TODO: write a test for article loadAll
+    //remember to reset anything that changed about your global
+    //environment at the end of the test!
+  });
+  QUnit.test('Article.allAuthors should get a unique name of authors', function(assert) {
+    //TODO: write a test for Article.allAuthors
+  });
+  //STRETCH: write a test for Article.numWordsAll
+  //STRETCH: write a test for Article.fetchAll
+})

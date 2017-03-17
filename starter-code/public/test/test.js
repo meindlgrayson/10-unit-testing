139f172633f46826cd42188beb184a5c77661916
F Grayson Meindl <meindlgrayson@gmail.com>
2017-03-17 14:53:00 -0700

    set up QUnit and knocked out first half of TODOs

--- starter-code/public/test/test.js
+++ starter-code/public/test/test.js
@@ -1,23 +1,69 @@
-QUnit.module('Article Constructor', function() {
-  QUnit.test('Article should create a new Arcticle', function(assert) {
-    //TODO: write a test for the Article constructor
+QUnit.module('Article Constructor', function () {
+  QUnit.test('Article should create a new Arcticle', function (assert) {
+    //DONE TODO: write a test for the Article constructor
+    var article = {
+      title: 'Transmitting Open-source Arrays',
+      category: 'firewall',
+      author: 'Dr. Tressie Kuphal',
+      authorUrl: 'http://http://corrine.net',
+      publishedOn: '2014-01-22',
+      body: '## The RSS sensor is down'
+    }
+
+    var thisGuy = new Article(article);
+    var result = thisGuy.category;
+    assert.equal(result, 'firewall');
   });
 
-  QUnit.test('Article instance should have a method toHtml', function(assert) {
-    //TODO: verify that an instance of article has a toHtml method
+  QUnit.test('Article instance should have a method toHtml', function (assert) {
+    //DONE TODO: verify that an instance of article has a toHtml method
+    var article = {
+      title: 'Transmitting Open-source Arrays',
+      category: 'firewall',
+      author: 'Dr. Tressie Kuphal',
+      authorUrl: 'http://http://corrine.net',
+      publishedOn: '2014-01-22',
+      body: '## The RSS sensor is down'
+    }
+    var thisGuy = new Article(article);
+    if (typeof thisGuy.toHtml === 'function'){
+      var result = true;
+    }
+
+    assert.equal(result, true);
+
   });
 });
 
-QUnit.module('Article Class', function() {
-  QUnit.test('Article.allArticles should be an array', function(assert) {
-    //TODO: write a test for the all articles property
+QUnit.module('Article Class', function () {
+  QUnit.test('Article.allArticles should be an array', function (assert) {
+    //DONE TODO: write a test for the all articles property
+    var article = {
+      title: 'Transmitting Open-source Arrays',
+      category: 'firewall',
+      author: 'Dr. Tressie Kuphal',
+      authorUrl: 'http://http://corrine.net',
+      publishedOn: '2014-01-22',
+      body: '## The RSS sensor is down'
+    };
+    var thisGuy = new Article(article);
+    Article.all.push(thisGuy)
+
+    if (Array.isArray(Article.all)){
+      var result = true;
+    }
+
+    assert.equal(result, true);
+
+
+
   });
-  QUnit.test('Article.loadAll should set an array of Article instances', function(assert) {
+  QUnit.test('Article.loadAll should set an array of Article instances', function (assert) {
     //TODO: write a test for article loadAll
     //remember to reset anything that changed about your global
     //environment at the end of the test!
   });
-  QUnit.test('Article.allAuthors should get unique author names', function(assert) {
+  QUnit.test('Article.allAuthors should get unique author names', function (assert) {
     //TODO: write a test for Article.allAuthors
   });
   //STRETCH: write a test for Article.numWordsAll
